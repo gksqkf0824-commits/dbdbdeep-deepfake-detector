88cf768c183cbdb49130c99eb953fefe276940d8
Ubuntu <ubuntu@ip-172-31-45-242.us-east-2.compute.internal>
2026-02-22 17:21:53 +0000

    fix: redis connect error & not used GPU

--- backend/redis_client.py
+++ backend/redis_client.py
@@ -1,4 +1,12 @@
+import os
 import redis
 
-# 로컬에 설치된 Redis에 연결 (Docker 등으로 띄웠을 때 기준)
-redis_db = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+redis_db = redis.StrictRedis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=0,
+    decode_responses=True
+)
